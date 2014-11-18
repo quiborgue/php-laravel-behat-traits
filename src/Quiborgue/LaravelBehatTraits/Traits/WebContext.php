2feda2c211dab2e3b892b31a165d516602f06017
Carlos Augusto Lima Borges <calimaborges@gmail.com>
2014-11-18 21:09:34 +0000

    Adding redirect validation to WebContext

--- src/Quiborgue/LaravelBehatTraits/Traits/WebContext.php
+++ src/Quiborgue/LaravelBehatTraits/Traits/WebContext.php
@@ -4,8 +4,10 @@ use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
 use Symfony\Component\DomCrawler\Crawler;
+use Illuminate\Http\RedirectResponse;
 
 trait WebContext {
+
     protected $web_response;
 
     /**
@@ -37,4 +39,23 @@ trait WebContext {
             throw new \Exception("Text '$text' was not found.");
         }
     }
+
+    /**
+     * @Then I should be on :arg1
+     */
+    public function iShouldBeOn($uri)
+    {
+        $response = $this->client->getResponse();
+
+        if (!is_a($response, 'Illuminate\Http\RedirectResponse')) {
+            throw new \Exception("Response is not Illuminate\Http\RedirectResponse");
+        }
+
+        $expected = $this->app['url']->to($uri);
+        $got = $response->headers->get('Location');
+        if ($expected != $got) {
+            throw new \Exception("Expected $expected got $got");
+        }
+    }
+
 }
